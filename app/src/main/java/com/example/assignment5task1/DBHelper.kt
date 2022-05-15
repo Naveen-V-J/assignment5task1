106e7e5c59a75f6e0a8616f16b1dbde20bfcb4a6
Naveen Jayakody <naveenjayakody12@gmail.com>
2022-05-15 18:17:08 +0800

    Created DBHelper.kt class. add, update, delete and get items functions implemented. Updated Item.kt to include additional constructors and dateBought parameter.

---
+++ app/src/main/java/com/example/assignment5task1/DBHelper.kt
@@ -0,0 +1,144 @@
+package com.example.assignment5task1
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import java.util.*
+
+class DBHelper(context:Context):SQLiteOpenHelper(context,DATABASE_NAME,null,DATABASE_VERSION) {
+
+    companion object{
+        private val DATABASE_NAME="ITEM_REPOSITORY"
+        private val DATABASE_VERSION=1
+        private val TABLE_NAME="Items"
+        private val ID="id"
+        private val NAME="name"
+        private val DETAILS="details"
+        private val QTY="qty"
+        private val SIZE="size"
+        private val URGENT="urgent"
+        private val BOUGHT="bought"
+        private val DATE_BOUGHT="date_bought"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val query="CREATE TABLE $TABLE_NAME($ID INTEGER PRIMARY KEY, $NAME TEXT, $DETAILS TEXT, $QTY INTEGER, $SIZE TEXT, $URGENT INTEGER, $BOUGHT INTEGER, $DATE_BOUGHT TEXT)"
+        db?.execSQL(query)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
+    }
+
+    fun insertItem(item:Item){
+        val db=this.writableDatabase
+        val contentValues=ContentValues()
+        contentValues.put(NAME,item.name)
+        contentValues.put(DETAILS,item.details)
+        contentValues.put(QTY,item.qty)
+        contentValues.put(SIZE,item.size)
+        contentValues.put(URGENT,item.urgent)
+        contentValues.put(BOUGHT,item.bought)
+
+        db.insert(TABLE_NAME,null,contentValues)
+        db.close()
+    }
+
+    fun updateItem(item: Item){
+        val db=this.writableDatabase
+        val contentValues=ContentValues()
+        contentValues.put(NAME,item.name)
+        contentValues.put(DETAILS,item.details)
+        contentValues.put(QTY,item.qty)
+        contentValues.put(SIZE,item.size)
+        contentValues.put(URGENT,item.urgent)
+        contentValues.put(BOUGHT,item.bought)
+
+        db.update(TABLE_NAME,contentValues,"$ID=?", arrayOf(item.id.toString()))
+        db.close()
+    }
+
+    fun updateItemBought(item: Item){
+        val db=this.writableDatabase
+        val contentValues=ContentValues()
+        contentValues.put(BOUGHT,item.bought)
+        contentValues.put(DATE_BOUGHT,item.dateBought)
+        db.update(TABLE_NAME,contentValues,"$ID=?", arrayOf(item.id.toString()))
+        db.close()
+    }
+
+    fun getAllItems():MutableList<Item>{
+        val db=this.readableDatabase
+        val itemList= mutableListOf<Item>()
+        val query="SELECT * FROM $TABLE_NAME"
+        val result=db.rawQuery(query,null)
+
+        if (result.moveToFirst()){
+            do{
+                val id=result.getInt(result.getColumnIndex(ID).toInt())
+                val name=result.getString(result.getColumnIndex(NAME).toInt())
+                val details=result.getString(result.getColumnIndex(ID).toInt())
+                val qty=result.getInt(result.getColumnIndex(ID).toInt())
+                val size=result.getString(result.getColumnIndex(ID).toInt())
+                val urgent=result.getInt(result.getColumnIndex(ID).toInt())
+                val bought=result.getInt(result.getColumnIndex(ID).toInt())
+                itemList.add(Item(id,name,details,qty,size, urgent,bought))
+            }while (result.moveToNext())
+        }
+        db.close()
+        return itemList
+    }
+
+    fun deleteItem(id:Int){
+        val db=this.writableDatabase
+        db.delete(TABLE_NAME,"$ID=?", arrayOf(id.toString()))
+        db.close()
+    }
+
+
+    fun getItemsWhereBought(isBought:Boolean):MutableList<Item>{
+        val db=this.readableDatabase
+        val itemList= mutableListOf<Item>()
+        val query="SELECT * FROM $TABLE_NAME WHERE $BOUGHT=$isBought"
+        val result=db.rawQuery(query,null)
+
+        if (result.moveToFirst()){
+            do{
+                val id=result.getInt(result.getColumnIndex(ID).toInt())
+                val name=result.getString(result.getColumnIndex(NAME).toInt())
+                val details=result.getString(result.getColumnIndex(ID).toInt())
+                val qty=result.getInt(result.getColumnIndex(ID).toInt())
+                val size=result.getString(result.getColumnIndex(ID).toInt())
+                val urgent=result.getInt(result.getColumnIndex(ID).toInt())
+                val bought=result.getInt(result.getColumnIndex(ID).toInt())
+                itemList.add(Item(id,name,details,qty,size, urgent,bought))
+            }while (result.moveToNext())
+        }
+        db.close()
+        return itemList
+    }
+
+    fun getItemsWhereUrgent(isUrgent:Boolean):MutableList<Item>{
+        val db=this.readableDatabase
+        val itemList= mutableListOf<Item>()
+        val query="SELECT * FROM $TABLE_NAME WHERE $URGENT=1"
+        val result=db.rawQuery(query,null)
+
+        if (result.moveToFirst()){
+            do{
+                val id=result.getInt(result.getColumnIndex(ID).toInt())
+                val name=result.getString(result.getColumnIndex(NAME).toInt())
+                val details=result.getString(result.getColumnIndex(ID).toInt())
+                val qty=result.getInt(result.getColumnIndex(ID).toInt())
+                val size=result.getString(result.getColumnIndex(ID).toInt())
+                val urgent=result.getInt(result.getColumnIndex(ID).toInt())
+                val bought=result.getInt(result.getColumnIndex(ID).toInt())
+                itemList.add(Item(id,name,details,qty,size, urgent,bought))
+            }while (result.moveToNext())
+        }
+        db.close()
+        return itemList
+    }
+}
--- app/src/main/java/com/example/assignment5task1/Item.kt
+++ app/src/main/java/com/example/assignment5task1/Item.kt
@@ -1,6 +1,10 @@
 package com.example.assignment5task1
 
-data class Item(
+data class Item(val id: Int,
     var name:String, var details:String, var qty:Int,
-    var size:String, var urgent:Boolean,
-    var bought:Boolean, val id: Int? =null)
+    var size:String, var urgent:Int,
+    var bought:Int, var dateBought:String){
+    constructor(id:Int, name: String,details: String,qty: Int,size: String,urgent: Int,bought: Int):this(id,name,details,qty,size,urgent,bought,"")
+    constructor(name: String,details: String,qty: Int,size: String,urgent: Int,bought: Int,dateBought: String):this(-1,name,details,qty,size,urgent,bought,dateBought)
+    constructor(name: String,details: String,qty: Int,size: String,urgent: Int,bought: Int):this(-1,name,details,qty,size,urgent,bought,"")
+}
